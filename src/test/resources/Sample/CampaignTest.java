e21a7dc481ee7cd419f063418322869e0ce45cd1
pavy114 <109330409+pavy114@users.noreply.github.com>
2022-07-18 16:57:37 +0530

    Revert "Amit campaign"

--- src/test/resources/Sample/CampaignTest.java
+++ src/test/resources/Sample/CampaignTest.java
@@ -3,30 +3,31 @@ package Sample;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
-public class CampaignTest {
-	
-	@Test(groups = "smoke")
-	public void createCampaign()
-	{
-		System.out.println("go to the campaign module");
-		System.out.println("click on the campaign module");
-	}
-
-	
-	@Test (groups = "regression")
-	public void createCampaignWithOrganization()
-	{
-		System.out.println("click on the campaign module");
-		System.out.println("inside the campaign module click on the organization module");
-		System.out.println("click on the organizzation module sucessfully");
-	}
-	
-	@Test (groups = {"smoke", "regression"})
-	public void createCampaignAndEdit()
-	{
-		System.out.println("create campaign done ");
-		System.out.println("click on the edit one ");
-		System.out.println("edit sucessfully...");
-		
-	}
-}
+//public class CampaignTest {
+//	
+//	@Test(groups = "smoke")
+//	public void createCampaign()
+//	{
+//		System.out.println("Inside Create Campaign");
+//		System.out.println("smoke test");
+//	}
+//
+//	
+//	@Test (groups = "regression")
+//	public void createCampaignWithOrganization()
+//	{
+//		System.out.println("Inside Create Campaign with Organization");
+//		System.out.println("regression test");
+//		System.out.println("regional testing1");
+//	}
+//	
+//	@Test (groups = {"smoke", "regression"})
+//	public void createCampaignAndEdit()
+//	{
+//		System.out.println("Inside Create Campaign And Edit");
+//		System.out.println("smoke test and regression");
+//		
+//	}
+//	
+//
+//}
