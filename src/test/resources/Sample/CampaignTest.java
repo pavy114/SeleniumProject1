93d863e18c53b500d2cae756998ed76eeba1b49e
hp <hp@AMIT-PC>
2022-07-18 17:06:29 +0530

    leads module

--- src/test/resources/Sample/CampaignTest.java
+++ src/test/resources/Sample/CampaignTest.java
@@ -28,5 +28,16 @@ public class CampaignTest {
 		System.out.println("click on the edit one ");
 		System.out.println("edit sucessfully...");
 		
+		System.out.println("amit kumar");
 	}
+	@Test (groups = {"smoke", "regression"})
+	public void  createlead()
+	{
+		System.out.println("create lead done ");
+		System.out.println(" leads is created sucessfully ");	
+		System.out.println("CHANGES DONE BY AMIT");
+		
+	}
+	
+
 }
