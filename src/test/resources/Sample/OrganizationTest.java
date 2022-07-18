567b6df44cc330e9a036fb235955409574511703
ADMIN <ADMIN@DESKTOP-VB164NU>
2022-07-18 12:55:54 +0530

    This is first commit

---
+++ src/test/resources/Sample/OrganizationTest.java
@@ -0,0 +1,33 @@
+package Sample;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+//public class OrganizationTest {
+//	
+//	@Test (groups = "smoke")
+//	public void createOrganization()
+//	{
+//		System.out.println("Inside Create Contact");
+//		System.out.println("smoke test");
+//	}
+//
+//	
+//	@Test (groups ="regression")
+//	public void createOrganizationWithVendor()
+//	{
+//		System.out.println("Inside Create Organization with Vendor");
+//		System.out.println("regression test");
+//		System.out.println("regional testing1");
+//		
+//	}
+//	
+//	@Test(groups = {"smoke", "regression"})
+//	public void createOrganizationAndEdit()
+//	{
+//		System.out.println("Inside Create Organization And Edit");
+//		System.out.println("smoke test and regression");
+//	}
+//	
+//
+//}
