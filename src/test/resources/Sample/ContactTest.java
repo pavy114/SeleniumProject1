68fef2eb6d2dfe09a77c9825faecc319222e9e0c
ADMIN <ADMIN@DESKTOP-VB164NU>
2022-07-19 17:36:50 +0530

    Changes made

--- src/test/resources/Sample/ContactTest.java
+++ src/test/resources/Sample/ContactTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class ContactTest {
 	
-	@Test (groups = "smoke")
+	@Test //(groups = "smoke")
 	public void createContact()
 	{
 		System.out.println("Inside Create Contact");
@@ -14,21 +14,21 @@ public class ContactTest {
 	}
 
 	
-	@Test (groups ="regression")
+	@Test // (groups ="regression")
 	public void createContactWithOrganization()
 	{
 		System.out.println("Inside Create Contact with Organization");
 		System.out.println("regression test");
 	}
 	
-	@Test(groups = {"smoke", "regression"})
+	@Test //(groups = {"smoke", "regression"})
 	public void createContactAndEdit()
 	{
 		System.out.println("Inside Create Contact And Edit");
 		System.out.println("smoke test and regression");
 	}
 	
-	@Test(groups = {"smoke", "regression"})
+	@Test //(groups = {"smoke", "regression"})
 	public void createContactwithIndustryt()
 	{
 		System.out.println("give industryhdjd name");
