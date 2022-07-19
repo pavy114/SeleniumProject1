84450fa40462d08efbbaaebdf50efa1e39dd3885
hp <hp@AMIT-PC>
2022-07-19 05:37:19 +0530

    One More class hs been created and added tp the project ......the class name is TeamMate

---
+++ src/test/resources/TeamMate.java
@@ -0,0 +1,13 @@
+
+public class TeamMeamber {
+
+	public static void main(String[] args) 
+	{
+
+	System.out.println("the total number of the team member is 24");
+	System.out.println("but in our project 2 automation test engineers and 1 test lead ");
+	System.out.println("the test lead name is Pavi.......");
+	System.out.println("the 2 automation engineers are 1)Amitkumar 2)shabnamswati..");
+	}
+
+}
